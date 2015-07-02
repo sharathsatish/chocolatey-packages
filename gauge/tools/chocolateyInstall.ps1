501857e20f18c609c5c7f673a17b12e2e8f025a5
sharaths <sharaths@thoughtworks.com>
2015-07-02 15:55:09 +0530

    Changing Gauge install to v0.1.5 - This now has silent uninstall working correctly

--- gauge/tools/chocolateyInstall.ps1
+++ gauge/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'gauge'
 $installerType = 'exe'
-$url = 'https://github.com/getgauge/gauge/releases/download/v0.1.4/gauge-0.1.4-windows.x86.exe'
-$url64 = 'https://github.com/getgauge/gauge/releases/download/v0.1.4/gauge-0.1.4-windows.x86_64.exe'
+$url = 'https://github.com/getgauge/gauge/releases/download/v0.1.5/gauge-0.1.5-windows.x86.exe'
+$url64 = 'https://github.com/getgauge/gauge/releases/download/v0.1.5/gauge-0.1.5-windows.x86_64.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
