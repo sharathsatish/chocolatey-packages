6389116524a96418ce1f3514fd2f612eed824bea
kashishm <kashishmunjal64@gmail.com>
2016-02-17 20:10:33 +0530

    changing update script

--- gauge/update.bat
+++ gauge/update.bat
@@ -1,7 +1,10 @@
-powershell "(Get-Content .\tools\chocolateyInstall.ps1) | ForEach-Object { $_ -replace '%1', '%2' } | Set-Content .\tools\chocolateyInstall.ps1"
-powershell "(Get-Content .\gauge.nuspec) | ForEach-Object { $_ -replace '%1', '%2' } | Set-Content .\gauge.nuspec"
+findstr "<version>" gauge.nuspec | cut -f2 -d "<" | cut -f 2 -d ">" > tmp.txt
+set /p old_version= < tmp.txt
+powershell "(Get-Content .\tools\chocolateyInstall.ps1) | ForEach-Object { $_ -replace '%old_version%', '%1' } | Set-Content .\tools\chocolateyInstall.ps1"
+powershell "(Get-Content .\gauge.nuspec) | ForEach-Object { $_ -replace '%old_version%', '%1' } | Set-Content .\gauge.nuspec"
 powershell "cpack"
-git add tools gauge.nuspec
-git commit -m "gauge %2"
-git push origin master
-choco push gauge.%2.nupkg
+#git add tools gauge.nuspec
+#git commit -m "gauge %2"
+#git push origin master
+#choco push gauge.%2.nupkg
+del tmp.txt
