f83d70054cf7d5e90354fbe5c0d5305ac224ffc1
kashishm <kashishmunjal64@gmail.com>
2016-02-18 15:17:59 +0530

    Commenting git related commands

--- gauge/update.bat
+++ gauge/update.bat
@@ -3,8 +3,8 @@ set /p old_version= < tmp.txt
 powershell "(Get-Content .\tools\chocolateyInstall.ps1) | ForEach-Object { $_ -replace '%old_version%', '%1' } | Set-Content .\tools\chocolateyInstall.ps1"
 powershell "(Get-Content .\gauge.nuspec) | ForEach-Object { $_ -replace '%old_version%', '%1' } | Set-Content .\gauge.nuspec"
 powershell "cpack"
-#git add tools gauge.nuspec
-#git commit -m "gauge %2"
-#git push origin master
-#choco push gauge.%2.nupkg
+@rem git add tools gauge.nuspec
+@rem git commit -m "gauge %2"
+@rem git push origin master
+@rem choco push gauge.%2.nupkg
 del tmp.txt
