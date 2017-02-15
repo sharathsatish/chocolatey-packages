f466b053904a27cbd5e5a8adef20eb5eb635fc3d
gaugeci <gaugeci@gmail.com>
2017-02-15 06:04:46 -0800

    gauge 0.8.0

--- gauge/tools/chocolateyInstall.ps1
+++ gauge/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'gauge'
 $installerType = 'exe'
-$url = 'https://github.com/getgauge/gauge/releases/download/v0.6.1/gauge-0.6.1-windows.x86.exe'
-$url64 = 'https://github.com/getgauge/gauge/releases/download/v0.6.1/gauge-0.6.1-windows.x86_64.exe'
+$url = 'https://github.com/getgauge/gauge/releases/download/v0.8.0/gauge-0.8.0-windows.x86.exe'
+$url64 = 'https://github.com/getgauge/gauge/releases/download/v0.8.0/gauge-0.8.0-windows.x86_64.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
