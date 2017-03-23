f7ab4adefddb127aeef44f1c201815b7a411b88f
sriv <srikanth.ddit@gmail.com>
2017-03-23 05:33:59 -0700

    fixed choco package, version in url was broken

--- gauge/tools/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-$packageName = 'gauge'
-$installerType = 'exe'
-$version = $env:version
-$url = "https://github.com/getgauge/gauge/releases/download/v{$version}/gauge-${version}-windows.x86.exe"
-$url64 = "https://github.com/getgauge/gauge/releases/download/v{$version}/gauge-${version}-windows.x86_64.exe"
-$silentArgs = '/S'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
