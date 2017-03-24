42c2ed679cd869ca937177942ff35902ac2e2fc7
Vinay Shankar Shukla <vinaysh@thoughtworks.com>
2017-03-24 11:48:42 +0530

    Update package.ps1

--- gauge/package.ps1
+++ gauge/package.ps1
@@ -1,4 +1,4 @@
-gc ..\version | %{
+gc ..\..\version | %{
     (Get-Content tools\chocolateyInstall.ps1.template).replace('{{version}}', $_) | Set-Content tools\chocolateyInstall.ps1;
     choco pack --version=$_
 }
